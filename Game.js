17dc3f6e2007e622ecf9df4d5336eaca6f36d2f9
judeallanhill <judeallanhill@icloud.com>
2023-05-28 15:41:46 -0700

    Keyboard Input, And finsihed setting up canvas

--- Game.js
+++ Game.js
@@ -1,21 +1,41 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let currentKey = new Map();
 class Player {
-
-}
-class Enemy {
     constructor(){
         this.x = 10
         this.y = 10
+        this.w = 10
+        this.h = 10
         this.alive = true;
     }
     draw() {
-                
+        ctx.fillRect(this.x,this.y,this.w,this.h);
     }
 }
-function Loop() {
+let player = new Player();
+function keyboardLoop() {
+    if (currentKey.get("w") === true) {
+        enemy.x += 10;
+    }
+}
+function keyboardInit() {
+    window.addEventListener("keydown", function (event) {
+        currentKey.set(event.key, true);
 
+    });
+    window.addEventListener("keyup", function (event) {
+        currentKey.set(event.key, false);
+    });
+}
+function Loop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    keyboardLoop();
+    player.draw();
     requestAnimationFrame(Loop)
 }
 function init() {
+    keyboardInit();
     Loop();
 }
 init();
