8498475154185ce4a02177e9168f231545836117
judeallanhill <judeallanhill@icloud.com>
2023-05-28 21:43:09 -0700

    Simple Player Movement and player Sprite

--- Game.js
+++ Game.js
@@ -1,12 +1,11 @@
 import {Rect} from "./RectUtils.js"
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
 let currentKey = new Map();
 class Player {
     constructor(){
         this.bounds = new Rect(10,10,10,10)
-        this.alive = true;
+        this.speed = 1;
     }
     draw() {
         ctx.fillRect(this.bounds.x,this.bounds.y,this.bounds.w,this.bounds.h);
@@ -14,8 +13,17 @@ class Player {
 }
 let player = new Player();
 function keyboardLoop() {
-    if (currentKey.get("w") === true) {
-        enemy.x += 10;
+    if (currentKey.get("w")) {
+        player.bounds.y -= player.speed;
+    }
+    if (currentKey.get("a")) {
+        player.bounds.x -= player.speed;
+    }
+    if (currentKey.get("s")) {
+        player.bounds.y += player.speed;
+    }
+    if (currentKey.get("d")) {
+        player.bounds.x += player.speed;
     }
 }
 function keyboardInit() {
@@ -28,9 +36,10 @@ function keyboardInit() {
     });
 }
 function Loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0,0,canvas.width,canvas.height);
     keyboardLoop();
     player.draw();
+    console.log("Running")
     requestAnimationFrame(Loop)
 }
 function init() {
